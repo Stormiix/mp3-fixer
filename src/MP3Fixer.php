0d4cd7d42bbb194ffea82a9d71048ed68921d8e5
Anas Mazouni <madadj4@gmail.com>
2018-05-26 12:23:06 +0000

    MP3Fixer Class & File iterator

---
+++ src/MP3Fixer.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * MP3Fixer main class | src/MP3Fixer.php
+ *
+ * @author Stormiix <madadj4@gmail.com>
+ * @license BSD 3-Clause License
+ * @version 0.0
+ * @copyright Copyright (c) 2018, Stormix.co
+ */
+
+namespace Stormiix\MP3Fixer;
+
+use Stormiix\EyeD3\EyeD3;
+
+/**
+*  MP3Fixer class
+*
+*  A PHP app to fix broken MP3 files ID3 Tags
+*
+*  @author Stormiix <madadj4@gmail.com>
+*  @license BSD 3-Clause License
+*  @version 0.0
+*
+*/
+
+// TODO: Use spotify API as a tag source
+// TODO: Use itunes API as a tag source
+// TODO: Use last API as a tag source
+
+class MP3Fixer
+{
+	/**
+    * Input directory that holds the broken MP3 files.
+    * @var string
+    */
+	public $inputFolder = "";
+
+	/**
+    * Output folder
+    * @var string
+    */
+	public $outputFolder = "";
+
+	public function __construct($inputFolder = "input",$outputFolder = "output")
+	{
+		$this->inputFolder = $inputFolder;
+		$this->outputFolder = $outputFolder;
+	}
+
+	public function isMP3($filename)
+	{
+		$finfo = finfo_open(FILEINFO_MIME_TYPE);
+		return (finfo_file($finfo, $filename) == "audio/mpeg");
+	}
+
+
+	public function getInputFiles()
+	{
+		try{
+			$iterator = new \DirectoryIterator($this->inputFolder);
+			$mp3s = [];
+			foreach ($iterator  as $fileInfo) {
+			    if ($fileInfo->isFile()){
+					if ($this->isMP3($fileInfo->getPathname())){
+						$mp3s[] = $fileInfo->getPathname();
+					}
+				}
+			}
+			return $mp3s;
+		}catch (\RuntimeException $e){
+			print("Input Folder (".$this->inputFolder.") can't be empty !");
+		}
+	}
+}
