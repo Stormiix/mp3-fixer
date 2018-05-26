9e5ce668502990f3f603381c1dc1d259c470dd52
Anas Mazouni <madadj4@gmail.com>
2018-05-26 12:24:44 +0000

    Apply fixes from StyleCI

--- src/app.php
+++ src/app.php
@@ -1,18 +1,17 @@
 <?php
 /**
- * MP3Fixer app | src/app.php
+ * MP3Fixer app | src/app.php.
  *
  * @author Stormiix <madadj4@gmail.com>
  * @license BSD 3-Clause License
+ *
  * @version 0.0
+ *
  * @copyright Copyright (c) 2018, Stormix.co
  */
-
 require '../vendor/autoload.php';
 
 use Stormiix\MP3Fixer\MP3Fixer;
 
-$fixer = new MP3Fixer("input","output");
+$fixer = new MP3Fixer('input', 'output');
 var_dump($fixer->getInputFiles());
-
-?>
